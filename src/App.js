0d169a2abf2670297564ca7397ff1c2facdd8c6e
i_vvvy <ivvvy.e@gmail.com>
2022-08-27 18:27:31 +0900

    React-router set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import MainPage from './page/MainPage';
+import DetailPage from './page/DetailPage';
+import NewPage from './page/NewPage';
+import EditPage from './page/EditPage';
+import MyListPage from './page/MyListPage';
 
 function App() {
   return (
-    <div className="App">
-      <h2>artistic moment</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/detail/:id" element={<DetailPage />}></Route>
+          <Route path="/new" element={<NewPage />}></Route>
+          <Route path="/edit/:id" element={<EditPage />}></Route>
+          <Route path="/myList" element={<MyListPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
